36d5da72740e84be1004859200fead38b042babf
nwbrady <nbrady@deepsig.io>
2025-08-30 08:50:45 -0400

    adjust clipboard so copying works from remote machines

--- init.lua
+++ init.lua
@@ -118,6 +118,21 @@ vim.schedule(function()
   vim.o.clipboard = 'unnamedplus'
 end)
 
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
+
+vim.g.loaded_clipboard_provider = nil
+vim.cmd("runtime autoload/provider/clipboard.vim")
+
 -- Enable break indent
 vim.o.breakindent = true
 
